880dc11a667aee7bf74b049c59039698fbcca975
Michael Becco <e4e6d4d5@gmail.com>
2015-09-18 09:44:47 -0700

    Add AllTimeLow to View and Pages Controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,5 +10,6 @@ class PagesController < ApplicationController
 
   def home
     @daily_ath_tally =  AllTimeHigh.daily_ath.daily_ath_count
+    @daily_atl_tally = AllTimeLow.daily_atl.daily_atl_count
   end
 end
