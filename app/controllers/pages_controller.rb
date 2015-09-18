a85365b4cb19d873b48f876eaa8b3612dfe9b423
Michael Becco <e4e6d4d5@gmail.com>
2015-09-18 11:03:24 -0700

    Add NewHigh and New Low Count, Modify Pages/Home View

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,5 +11,7 @@ class PagesController < ApplicationController
   def home
     @daily_ath_tally =  AllTimeHigh.daily_ath.daily_ath_count
     @daily_atl_tally = AllTimeLow.daily_atl.daily_atl_count
+    @daily_nh_tally = NewHigh.daily_nh.daily_nh.count
+    @daily_nl_tally = NewLow.daily_nl.daily_nl.count
   end
 end
--- app/models/new_low.rb
+++ app/models/new_low.rb
@@ -1,4 +1,7 @@
 class NewLow < ActiveRecord::Base
   validates :symbol, :saved_on, presence: true
   validates :symbol, uniqueness: { scope: :saved_on }
+
+  scope :daily_nl, -> { where("saved_on >=?", Time.zone.now.beginning_of_day) }
+  scope :daily_nl_count, -> { self.select(:saved_on).group(:saved_on).to_a.count }
 end
