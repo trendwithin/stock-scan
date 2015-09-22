d8fe7300eba2756166c232283845f18aaa46a5fd
Michael Becco <e4e6d4d5@gmail.com>
2015-09-22 11:26:05 -0700

    Add Helpers for Charts

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,9 @@
 module PagesHelper
+  def highs_minus_lows
+    @highs.group_by_day(:saved_on, format: "%B %d, %Y").count.merge(@lows.group_by_day(:saved_on, format: "%B %d, %Y").count){ |key, first, second| first - second}
+  end
+
+  def chart_highs
+    NewHigh.pluck(:symbol).map { |e| { name: e, data: @highs.where(symbol: e).group_by_day(:saved_on, format: "%b %d, %Y").count } }
+  end
 end
